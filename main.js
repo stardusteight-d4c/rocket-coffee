7a84e9f88d42f8f7837e8a41f09ddeb10b0ccaac
Gabriel <bellsena@outlook.com>
2022-06-15 19:44:23 -0300

    ajustes no menu mobile, button e title

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ menuOff = true;
 menuToggle.addEventListener("click", (event) => {
   if (menuOff) {
     itemsMenu.classList.add("menu__items--active");
+    menuToggle.setAttribute('src', 'assets/menu-buguer-close.svg');
     menuOff = false;
     ocultarDiv(".banner__content");
     ocultarDiv(".blur-mobile");
   } else {
     itemsMenu.classList.remove("menu__items--active");
+    menuToggle.setAttribute('src', 'assets/menu-buguer-open.svg');
     menuOff = true;
     exibirDiv(".banner__content");
     exibirDiv(".blur-mobile");
